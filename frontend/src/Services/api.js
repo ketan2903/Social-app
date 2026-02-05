1dd96d4534a64baadfa2f659cc036a00f7864313
ketan2903 <ketandesale2004@gmail.com>
2026-02-05 18:19:17 +0530

    Update API URL for deployment

--- frontend/src/Services/api.js
+++ frontend/src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://social-app-1-raj4.onrender.comapi"
+  baseURL: "https://social-app-1-raj4.onrender.com"
 });
 
 API.interceptors.request.use((req) => {
